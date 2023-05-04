24f0d4d55fad28826493e3c11a4722dd84ea7469
Dorabang <psa1195@naver.com>
2023-05-04 23:31:04 +0900

    feat: Footer 내용 추가

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,14 +1,31 @@
-import Logo from '../../images/logo.png';
+import Link from 'next/link';
+import { Noto_Serif_KR } from 'next/font/google';
+import Github from '../../images/Github_logo.png';
 import Image from 'next/image';
+
+const notoSerifKR = Noto_Serif_KR({
+  weight: ['400', '700', '900'],
+  subsets: ['latin'],
+});
 
 const Footer = () => {
   return (
-    <footer>
-      <div className='container mx-auto flex justify-center flex-wrap'>
-        <div className='w-full text-center'>
-          <Image src={Logo} alt='semicolon 로고 이미지' className='mx-auto' />
+    <footer className='border-t-2 border-solid border-black'>
+      <div className='container mx-auto py-12 flex justify-between items-center'>
+        <address>
+          <span className={`${notoSerifKR.className} font-bold`}>
+            (FrontEnd Developer) Park su-a
+          </span>
+          <br />
+          <span className='pt-5 pb-2.5 inline-block'>Tel : 010.8930.1195</span>
+          <br />
+          E-mail : psa1195@naver.com
+        </address>
+        <div>
+          <Link href='https://github.com/Dorabang'>
+            <Image src={Github} alt='Github 로고 이미지' />
+          </Link>
         </div>
-        <address className='text-gray-500'>Email : psa1195@naver.com</address>
       </div>
     </footer>
   );
