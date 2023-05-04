f7814cf31f28757de167963a9f36450cd569553e
Dorabang <psa1195@naver.com>
2023-05-04 19:33:13 +0900

    feat: layout 변경 WIP

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import './globals.css';
+import { Noto_Sans_KR } from 'next/font/google';
+import Head from 'next/head';
+
+const notoSansKR = Noto_Sans_KR({
+  weight: ['400', '700', '900'],
+  subsets: ['latin'],
+});
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+export const metadata = {
+  title: 'Sem;Colon',
+  description: 'Park Su-a Web Development Portfolio',
+  keywords:
+    'web development, web design, javascript, react, nextjs, node, html, css',
+};
+
+export default function RootLayout({ children }: LayoutProps) {
+  return (
+    <html lang='ko'>
+      <Head>
+        <link rel='shorcut icon' href='/favicon.ico' />
+      </Head>
+      <body className={notoSansKR.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
