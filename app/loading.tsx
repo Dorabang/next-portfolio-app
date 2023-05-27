15e3caaefda08decc1883cbb3202b2b70a3403a4
Dorabang <psa1195@naver.com>
2023-05-27 15:48:35 +0900

    feat: 로딩 페이지

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+const LoadingPage = () => {
+  return (
+    <>
+      <div className='text-center flex justify-center items-center h-screen'>
+        <div className='animate-spin rounded-full w-10 h-10 border-4 border-solid border-neutral-900/10 border-l-neutral-900 dark:border-white/10 dark:border-l-neutral-50'></div>
+      </div>
+    </>
+  );
+};
+
+export default LoadingPage;
