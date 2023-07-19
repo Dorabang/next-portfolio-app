51956e57cd0c1224e7b002bc0eadc80a7ea4654c
Dorabang <psa1195@naver.com>
2023-07-19 23:13:09 +0900

    chore: 깃헙 링크 버튼 위치 수정

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -5,24 +5,22 @@ import { FaGithubSquare } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className='border-t-2 border-solid border-black dark:border-zinc-500'>
-      <div className='container mx-auto py-12 flex justify-between items-center xl:px-0 px-4'>
-        <address className={`${notoSerifKR.className} dark:text-zinc-400`}>
+    <footer className='border-t border-solid bg-zinc-900 text-white border-black dark:border-zinc-500'>
+      <div className='container mx-auto py-12 xl:px-0 px-4 relative'>
+        <Link
+          href='https://github.com/Dorabang'
+          target='_blank'
+          className='dark:text-zinc-400'
+        >
+          <FaGithubSquare size={36} />
+        </Link>
+        <address className={`${notoSerifKR.className} dark:text-zinc-400 pt-4`}>
           <span className='font-bold'>(FrontEnd Developer) Park su-a</span>
           <br />
           <span className='pt-5 pb-2.5 inline-block'>Tel : 010.8930.1195</span>
           <br />
           E-mail : psa1195@naver.com
         </address>
-        <div>
-          <Link
-            href='https://github.com/Dorabang'
-            target='_blank'
-            className='dark:text-zinc-400'
-          >
-            <FaGithubSquare size={36} />
-          </Link>
-        </div>
       </div>
     </footer>
   );
