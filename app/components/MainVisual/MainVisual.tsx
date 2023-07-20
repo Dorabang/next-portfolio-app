8bdd4d7f7773b14471c9591ce83d2083e72e7f0e
Dorabang <psa1195@naver.com>
2023-07-20 17:56:36 +0900

    chore: 메인 슬라이더 미디어쿼리 추가 및 슬라이드 추가

--- app/components/MainVisual/MainVisual.tsx
+++ app/components/MainVisual/MainVisual.tsx
@@ -1,6 +1,8 @@
 'use client';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
+import Image from 'next/image';
+import Link from 'next/link';
 
 // Import Swiper styles
 import './MainVisual.css';
@@ -11,9 +13,18 @@ import 'swiper/css/navigation';
 // import required modules
 import { Pagination, Navigation } from 'swiper';
 
+// import mv img
 import mv_img1 from 'public/images/mv_undefined.jpg';
 import mv_img2 from 'public/images/mv_todoSomething.jpg';
-import Image from 'next/image';
+import mv_img3 from 'public/images/mv_disney.jpg';
+
+import mv_img1_tablet from 'public/images/mv_undefined_tablet.jpg';
+import mv_img2_tablet from 'public/images/mv_todoSomething_tablet.jpg';
+import mv_img3_tablet from 'public/images/mv_disney_tablet.jpg';
+
+import mv_img1_mobile from 'public/images/mv_undefined_mobile.jpg';
+import mv_img2_mobile from 'public/images/mv_todoSomething_mobile.jpg';
+import mv_img3_mobile from 'public/images/mv_disney_mobile.jpg';
 
 const MainVisual = () => {
   return (
@@ -33,10 +44,61 @@ const MainVisual = () => {
         modules={[Pagination, Navigation]}
       >
         <SwiperSlide>
-          <Image src={mv_img1} alt='Undefined 메인 배너 이미지' />
+          <Link href={'/portfolio/0'}>
+            <Image
+              src={mv_img1}
+              alt='Undefined 메인 배너 이미지'
+              className='lg:block hidden'
+            />
+            <Image
+              src={mv_img1_tablet}
+              alt='Undefined 메인 배너 이미지'
+              className='hidden sm:block lg:hidden'
+            />
+            <Image
+              src={mv_img1_mobile}
+              alt='Undefined 메인 배너 이미지'
+              className='block sm:hidden'
+            />
+          </Link>
         </SwiperSlide>
         <SwiperSlide>
-          <Image src={mv_img2} alt='Todo Something 메인 배너 이미지' />
+          <Link href={'/portfolio/3'}>
+            <Image
+              src={mv_img2}
+              alt='Todo Something 메인 배너 이미지'
+              className='lg:block hidden'
+            />
+            <Image
+              src={mv_img2_tablet}
+              alt='Todo Something 메인 배너 이미지'
+              className='hidden sm:block lg:hidden'
+            />
+            <Image
+              src={mv_img2_mobile}
+              alt='Todo Something 메인 배너 이미지'
+              className='block sm:hidden'
+            />
+          </Link>
+        </SwiperSlide>
+        <SwiperSlide>
+          <Link href={'/portfolio/2'}>
+            <Image
+              src={mv_img3}
+              alt='Disney Plus 메인 배너 이미지'
+              className='lg:block hidden'
+            />
+            <Image
+              src={mv_img3_tablet}
+              alt='Disney Plus 메인 배너 이미지'
+              className='hidden sm:block lg:hidden'
+            />
+            <Image
+              src={mv_img3_mobile}
+              alt='Disney Plus 메인 배너 이미지'
+              className='block sm:hidden'
+            />
+          </Link>
         </SwiperSlide>
       </Swiper>
     </section>
