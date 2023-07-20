a23e8514f754b4398afb97ce7ba2cf3e73dee64d
Dorabang <psa1195@naver.com>
2023-07-20 17:56:57 +0900

    chore: 새로운 포트폴리오 이미지 추가

--- app/components/Portfolio/Constans.ts
+++ app/components/Portfolio/Constans.ts
@@ -1,29 +1,45 @@
 import { StaticImageData } from 'next/image';
+
+// thumb image1
 import romand from 'public/images/romand_thumb.jpg';
-import romand_concept from 'public/images/romand_concept.jpg';
 import movieApp from 'public/images/movieApp_thumb.jpg';
-import movieApp_concept from 'public/images/movieApp_concept.jpg';
 import todoSomething from 'public/images/todoSomething_thumb.jpg';
-import todoSomething_concept from 'public/images/todoSomething_concept.jpg';
+import disney from 'public/images/disney_thumb.jpg';
 import semicolon from 'public/images/semicolon_thumb.jpg';
-import semicolon_concept from 'public/images/semicolon_thumb.jpg';
 import UndefinedImg from 'public/images/Undefined_thumb.jpg';
+
+// thumb image2
+import romand2 from 'public/images/romand_thumb2.jpg';
+import movieApp2 from 'public/images/movieApp_thumb2.jpg';
+import todoSomething2 from 'public/images/todoSomething_thumb2.jpg';
+import disney2 from 'public/images/disney_thumb2.jpg';
+import semicolon2 from 'public/images/semicolon_thumb2.jpg';
+import UndefinedImg2 from 'public/images/Undefined_thumb2.jpg';
+
+// concept images
+import romand_concept from 'public/images/romand_concept.jpg';
+import movieApp_concept from 'public/images/movieApp_concept.jpg';
+import todoSomething_concept from 'public/images/todoSomething_concept.jpg';
+import disney_concept from 'public/images/disney_concept.jpg';
+import semicolon_concept from 'public/images/semicolon_thumb.jpg';
 import Undefined_webMall_concept from 'public/images/Undefined_webMall_concept.jpg';
+
 export interface projectsType {
   id: number;
   name: string;
-  concept: string;
   descImg: StaticImageData;
   image: StaticImageData;
+  image2: StaticImageData;
   category: string;
   link: string;
 }
+
 export const projects: projectsType[] = [
   {
     id: 0,
     name: 'Undefined',
-    concept: 'Undefined_webMall',
     image: UndefinedImg,
+    image2: UndefinedImg2,
     descImg: Undefined_webMall_concept,
     category: 'NextJS',
     link: 'https://web-mall.vercel.app/',
@@ -31,35 +47,44 @@ export const projects: projectsType[] = [
   {
     id: 1,
     name: 'sem;-colon',
-    concept: 'semicolon',
     image: semicolon,
+    image2: semicolon2,
     descImg: semicolon_concept,
     category: 'NextJS',
     link: '/',
   },
   {
     id: 2,
+    name: 'DisneyPlus',
+    image: disney,
+    image2: disney2,
+    descImg: disney_concept,
+    category: 'React',
+    link: 'https://react-disney-plus-app-92536.firebaseapp.com/',
+  },
+  {
+    id: 3,
     name: 'TodoSomething',
-    concept: 'todoSomething',
     image: todoSomething,
+    image2: todoSomething2,
     descImg: todoSomething_concept,
     category: 'React',
     link: 'https://dorabang.github.io/wanted-pre-onboarding-frontend/',
   },
   {
-    id: 3,
-    name: 'movieApp',
-    concept: 'movieApp',
+    id: 4,
+    name: 'movieList',
     image: movieApp,
+    image2: movieApp2,
     descImg: movieApp_concept,
     category: 'React',
     link: 'https://dorabang.github.io/movieApp/',
   },
   {
-    id: 4,
+    id: 5,
     name: 'rom&nd,',
-    concept: 'romand',
     image: romand,
+    image2: romand2,
     descImg: romand_concept,
     category: 'HTML&CSS',
     link: 'https://dorabang.github.io/romandRenewalProject/',
