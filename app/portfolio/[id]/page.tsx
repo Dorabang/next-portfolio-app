5d5374ce0dafaeca4b983fa48e83b962dfa9d67e
Dorabang <psa1195@naver.com>
2023-07-30 20:41:14 +0900

    feat: 메타데이터 추가 및 수정 진행 중

--- app/portfolio/[id]/page.tsx
+++ app/portfolio/[id]/page.tsx
@@ -5,9 +5,23 @@ import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import notoSerifKR from '@/app/components/NotoSerif';
 import { BsBoxArrowUpRight } from 'react-icons/bs';
+import Head from 'next/head';
+import { Metadata, ResolvingMetadata } from 'next';
+import NotFound from '@/app/not-found';
 
 interface paramsType {
   params: { id: number };
+}
+
+export async function generateMetadata({
+  params: id,
+}: paramsType): Promise<Metadata> {
+  const project = projects.find((item) => item.id === Number(id));
+  console.log(project);
+
+  return {
+    title: `Sem;Colon || ${project?.name}`,
+  };
 }
 
 const Wrapper = ({ children }: { children: React.ReactNode }) => {
@@ -29,26 +43,33 @@ const PortfolioPage = ({ params: { id } }: paramsType) => {
     const result = projects.find((item) => item.id === Number(id));
     setProject(result);
   }, [id]);
-  if (!project) return null;
+  if (!project) return <NotFound />;
 
   return (
-    <div className='container mx-auto h-full'>
-      <div className='relative w-full'>
-        <Image
-          src={project.descImg}
-          alt={`${project.name} 컨셉 페이지 이미지`}
-        />
+    <>
+      <Head>
+        <title>Sem;Colon || {project.name}</title>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <div className='container mx-auto h-full'>
+        <div className='relative w-full'>
+          <Image
+            src={project.descImg}
+            alt={`${project.name} 컨셉 페이지 이미지`}
+          />
+        </div>
+        <Wrapper>
+          <Link
+            href={`${project.link}`}
+            target='_blank'
+            className={`${notoSerifKR.className} text-xs lg:p-3 p-2`}
+          >
+            <BsBoxArrowUpRight size={22} className='scale-[0.82]' />
+          </Link>
+        </Wrapper>
       </div>
-      <Wrapper>
-        <Link
-          href={`${project.link}`}
-          target='_blank'
-          className={`${notoSerifKR.className} text-xs lg:p-3 p-2`}
-        >
-          <BsBoxArrowUpRight size={22} className='scale-[0.82]' />
-        </Link>
-      </Wrapper>
-    </div>
+    </>
   );
 };
 export default PortfolioPage;
