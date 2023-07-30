6bb016328851dd2eda7717891062e4e7007b8099
Dorabang <psa1195@naver.com>
2023-07-30 17:50:07 +0900

    chore: 메타데이터 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,6 @@ import AboutMain from './components/AboutMain/AboutMain';
 import MainVisual from './components/MainVisual/MainVisual';
 import Portfolio from './components/Portfolio/Portfolio';
 
-export const metadata = {
-  title: 'Sem;Colon',
-  description: 'Park Su-a Web Development Portfolio',
-  keywords:
-    'web development, web design, javascript, react, nextjs, node, html, css',
-};
-
 const Home = () => {
   return (
     <main>
